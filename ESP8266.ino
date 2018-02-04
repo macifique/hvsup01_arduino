f82219500f2d5b749e7cef4ab9de48d9fb07cf5b
macifique <36132622+macifique@users.noreply.github.com>
2018-02-04 17:06:05 +0100

    Update ESP8266.ino

--- ESP8266.ino
+++ ESP8266.ino
@@ -8,26 +8,17 @@ void serve();
 void getter();
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Starting...");
-  radcounter.begin(400000);
-  getter();
   
-  attachInterrupt(0, serve, FALLING);
-  serve();
-}
-
-void loop() {
-  
-  //delay(2000);                  // waits for a second
-  //getter();
-
-}
-void getter() {
   unsigned long serial_no;
   unsigned short hw_no, fw_no;
   
-  radcounter.getinfo(serial_no, fw_no, hw_no);
+  Serial.begin(9600);
+  Serial.println("Starting...");
+  
+  radcounter.begin(400000); // Initialize hvsup01 with 400kHz clock
+  
+  radcounter.getinfo(serial_no, fw_no, hw_no);  // Get factory data
+  
   Serial.println("Rad Counter device info:");
   Serial.print("Firmware Version: ");
   Serial.println(fw_no, HEX);
@@ -35,24 +26,29 @@ void getter() {
   Serial.println(hw_no, HEX);
   Serial.print("SerialNo: ");
   Serial.println(serial_no, HEX);
+  
+  attachInterrupt(0, serve, FALLING); // Atach interrupt of TRIG pin to 'serve'
+  serve();  // ...and call serve first directly, to perform the initial setup
+}
+
+void loop() {
+  // We use periodic interrupt, so nothing is here
 }
 
 void serve() {
-  int incomingByte = 0;
   unsigned long pulsecount;
   unsigned short voltage;
   unsigned short timeout;
 
-  //delay(50);
-  radcounter.getparams(pulsecount, voltage, timeout);
-  if (voltage != vset) {
+  radcounter.getparams(pulsecount, voltage, timeout); //get measurement data and settings
+  if (voltage != vset) {  // check if setting is correct. This code runs normally once at startup
     Serial.print("Voltage mismatch. Read out voltage is:");
     Serial.println(voltage);
     Serial.print("Setting voltage to ");
     Serial.print(vset);
     Serial.println("V ...");
     
-    radcounter.setparams(vset, tset);
+    radcounter.setparams(vset, tset); // Set the tube voltage and timeout
   } else {
     Serial.print("Pulsecount: ");
     Serial.println(pulsecount);
